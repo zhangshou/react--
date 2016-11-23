d0ab6617581fc682baf04252b2e0954a39c840a1
zhangshou <83363694@qq.com>
2016-11-23 11:03:15 +0800

    'jsx语法'

--- js/1.react.js
+++ js/1.react.js
@@ -1,12 +1,15 @@
 //react.js React
 //react-dom.js ReactDom
 //jsx是一种将js和html代码混合起来的一种写法
-ReactDOM.render(
+/*ReactDOM.render(
     <h1>hello</h1>,
     document.getElementById('app')
-);
+);*/
 
+var count=0;
 function render(ele,container){
     container.innerHTML=ele;
 }
-render('<h1>hello</h1>',document.getElementById('app'))
+document.body.onclick= function () {
+    render('<h1>'+(++count)+'</h1>',document.getElementById('app'))
+};
---
+++ js/2.jsx.js
@@ -0,0 +1,17 @@
+/*是一种JS和HTML混合的语法,将组件的结构、数据甚至样式都聚合在一起定义组件,会编译成普通的Javascript。
+
+    遇到HTML标签(以 < 开头)，就用HTML规则解析
+    遇到代码块(以 { 开头)，就用JavaScript规则解析
+    使用样式时可以让style等于一个样式对象
+    使用样式类时只能使用className=类名,因为class是Javascript关键字*/
+
+var names=['梅西','内马尔','苏亚雷斯'];
+ReactDOM.render(
+    <nl>
+        {
+            names.map(function (item) {
+                return <li>{item}</li>
+            })
+        }
+    </nl>,document.getElementById('app')
+);
