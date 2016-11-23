d25753776377664581eb74427b895f90374770e5
zhangshou <83363694@qq.com>
2016-11-23 16:17:46 +0800

    '表单双向数据绑定'

---
+++ js/8.compound.js
@@ -0,0 +1,21 @@
+/*
+* 1.定义初始化状态
+* 2.给input绑定onChange事件,在方法里通过形参event得到event.target.value,从而得到input框的值
+* */
+var Input=React.createClass({
+    getInitialState(){
+        return {val:''}
+    },
+    handleChange(event){
+        this.setState({val:event.target.value})
+    },
+    render(){
+        return (
+            <div>
+                <input type="text" value={this.state.val} onChange={this.handleChange}/>
+                <p>{this.state.val}</p>
+            </div>
+        )
+    }
+});
+ReactDOM.render(<Input/>,document.getElementById('app'));
